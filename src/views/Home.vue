c048e856cad972cdafbf25df52030ab21a08871d
Odimaa <odimaa.horsfall@gmail.com>
2021-01-05 17:45:20 +0100

    add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,682 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <section class="contain-shape1" id="hero">
+      <div class="row">
+        <div class="col-lg-6 mt-5">
+          <div class="text mt-5">
+            <h3>Hello, I'm</h3>
+            <h1><span class="line">Odi</span>maa Allen</h1>
+            <h1>Frontend Developer</h1>
+          </div>
+        </div>
+        <div class="col-lg-6" id="portfolio-img">
+          <img src="images/portfolio.svg" />
+        </div>
+      </div>
+
+      <div class="custom-shape-divider-bottom-1609841878">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+    </section>
+
+    <section class="contain-shape2" id="in-touch">
+      <div class="row p-5">
+        <div class="col-md-8 col-lg-8">
+          <h5 style="color: fff">
+            I'm a tenacious programmer who uses continuous iteration to produce
+            results quickly and continuously improve myself.
+          </h5>
+        </div>
+        <div class="col-md-4 col-lg-4 mt-2">
+          <a href="images/CV.pdf" class="btn btn-primary">Download CV &rarr;</a>
+        </div>
+      </div>
+      <div class="custom-shape-divider-bottom-1609841511">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+    </section>
+
+    <section class="contain-shape3" id="project">
+      <div class="section-title">
+        <h2>Services</h2>
+        <p>What I Do</p>
+      </div>
+
+      <div class="container py-5">
+        <div class="row align-items-center py-5">
+          <div class="col-md-12 col-lg-4">
+            <h2 class="mb-4 weight-600">My Services</h2>
+            <p class="text-muted mb-5">Road to Fullstack Development</p>
+          </div>
+          <div class="col-md-12 col-lg-8">
+            <div class="row">
+              <div class="col-6">
+                <div class="bg-new p-5 text-white">
+                  <h4 class="bg-heading">#Skills</h4>
+                  <ul class="list-unstyled uu">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>Vue.js</li>
+                    <li>React</li>
+                    <li>Bootsrap</li>
+                    <li>Tailwind</li>
+                    <li>Github</li>
+                    <li>Gitlab</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-6">
+                <div class="bg-new-2 p-5 text-white">
+                  <h4 class="bg-heading2">#learning</h4>
+                  <ul class="list-unstyled uu">
+                    <li>Laravel</li>
+                    <li>Node.js</li>
+                    <li>Wordpress</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="contain-shape5" id="about">
+      <div class="section-title">
+        <h2>Portfolio</h2>
+        <p>Recent Projects</p>
+
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="cards">
+              <div class="card card1">
+                <div class="container">
+                  <img src="images/consult.jpg" />
+                </div>
+                <div class="details">
+                  <h3>Consulting company</h3>
+                  <p>Type: Landing page</p>
+                  <p>Html, CSS, Javascript</p>
+                  <a
+                    href="https://cushion-cloud.netlify.app/"
+                    class="btn btn-primary"
+                    target="_blank"
+                    >View project &rarr;</a
+                  >
+                </div>
+              </div>
+              <div class="card card2">
+                <div class="container">
+                  <img src="images/scarlett.jpg" />
+                </div>
+                <div class="details">
+                  <h3>Recipe Retrieval</h3>
+                  <p>Type: Application</p>
+                  <p>Reactjs</p>
+                  <a
+                    href="https://allrecipesodimaa.netlify.app"
+                    class="btn btn-primary"
+                    target="_blank"
+                    >View project &rarr;</a
+                  >
+                </div>
+              </div>
+              <div class="card card3">
+                <div class="container">
+                  <img src="images/restaurant.jpg" />
+                </div>
+                <div class="details">
+                  <h3>Restaurant</h3>
+                  <p>Type: Landing page</p>
+                  <p>HTML, CSS</p>
+                  <a
+                    href="
+https://noirodi.netlify.app/"
+                    class="btn btn-primary"
+                    target="_blank"
+                    >View project &rarr;</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-12">
+            <div class="cards">
+              <div class="card card3">
+                <div class="container">
+                  <img src="images/job.jpg" />
+                </div>
+                <div class="details">
+                  <h3>Job Board</h3>
+                  <p>Type: Application</p>
+                  <p>Reactjs</p>
+                  <a
+                    href="https://odi-react-job.netlify.app"
+                    class="btn btn-primary"
+                    target="_blank"
+                    >View project &rarr;</a
+                  >
+                </div>
+              </div>
+              <div class="card card1">
+                <div class="container">
+                  <img src="images/noir.jpg" />
+                </div>
+                <div class="details">
+                  <h3>Digital Agency</h3>
+                  <p>Type: Landing page</p>
+                  <p>Html, CSS, Javascript</p>
+                  <a
+                    href="https://p3solutions.netlify.app/"
+                    class="btn btn-primary"
+                    target="_blank"
+                    >View project &rarr;</a
+                  >
+                </div>
+              </div>
+              <div class="card card2">
+                <div class="container">
+                  <img src="images/bakery.jpg" />
+                </div>
+                <div class="details">
+                  <h3>Bakery</h3>
+                  <p>Type: Landing page</p>
+                  <p>HTML, CSS , Javascript</p>
+                  <a
+                    href="https://scarlett-confectionary.netlify.app/"
+                    class="btn btn-primary"
+                    target="_blank"
+                    >View project &rarr;</a
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="contain-shape6" id="get-in">
+      <div class="contact-me">
+        <div class="section-title">
+          <p style="color: #fff">Get in Touch</p>
+        </div>
+        <div class="row mt-5">
+          <div class="col-md-6 col-lg-4">
+            <div class="icon-box">
+              <div class="icon">
+                <img
+                  src="https://img.icons8.com/doodle/96/000000/address.png"
+                />
+              </div>
+              <p class="description">Lagos, Nigeria</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4">
+            <div class="icon-box">
+              <div class="icon">
+                <img
+                  src="https://img.icons8.com/doodle/96/000000/email--v1.png"
+                />
+              </div>
+              <p class="description">Odimaa.horsfall@gmail.com</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4">
+            <div class="icon-box">
+              <div class="icon">
+                <img
+                  src="https://img.icons8.com/doodle/96/000000/phone--v1.png"
+                />
+              </div>
+              <p class="description">+234 815 931 9442</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="custom-shape-divider-top-1609846653">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="shape-fill"></path>
+        </svg>
+      </div>
+
+      <!-- End Contact Section -->
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+export default {};
 </script>
+
+<style scoped>
+* {
+  margin: 0;
+  overflow: hidden;
+  padding: 0;
+}
+
+.text {
+  margin-top: 20px;
+}
+
+.btn {
+  padding: 13.5px 36px;
+  font-weight: 700;
+  border-radius: 30px;
+  font-size: 13px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+}
+.btn-primary {
+  background-color: #6264fa;
+}
+.btn-primary:hover {
+  background-color: #3b3dd1;
+}
+
+#portfolio-img img {
+  width: 560px;
+  height: 560px;
+}
+
+.course {
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2);
+  display: flex;
+  max-width: 100%;
+  margin: 20px;
+  overflow: hidden;
+  width: 700px;
+}
+
+.course h6 {
+  opacity: 0.6;
+  margin: 0;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+}
+
+.course h2 {
+  letter-spacing: 1px;
+  margin: 10px 0;
+}
+
+.course-preview {
+  background-color: #2a265f;
+  color: #fff;
+  padding: 30px;
+  max-width: 250px;
+}
+
+.course-preview a {
+  color: #fff;
+  display: inline-block;
+  font-size: 12px;
+  opacity: 0.6;
+  margin-top: 30px;
+  text-decoration: none;
+}
+
+.course-info {
+  padding: 30px;
+  position: relative;
+  width: 100%;
+}
+
+.btn-2 {
+  background-color: #2a265f;
+  border: 0;
+  border-radius: 50px;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2);
+  color: #fff;
+  font-size: 16px;
+  padding: 12px 25px;
+  position: absolute;
+  bottom: 30px;
+  right: 30px;
+  letter-spacing: 1px;
+}
+/*--------------------------------------------------------------
+# Sections General
+--------------------------------------------------------------*/
+section {
+  padding: 60px 0;
+}
+
+.section-bg {
+  background-color: #fef8f5;
+}
+
+.section-title {
+  text-align: center;
+  padding-bottom: 30px;
+}
+
+.section-title h2 {
+  font-size: 24px;
+  font-weight: 700;
+  padding-bottom: 0;
+  line-height: 1px;
+  margin-bottom: 15px;
+  color: #c2b7b1;
+}
+
+.section-title p {
+  padding-bottom: 15px;
+  margin-bottom: 15px;
+  position: relative;
+  font-size: 32px;
+  font-weight: 700;
+  color: #4e4039;
+}
+
+.section-title p::after {
+  content: "";
+  position: absolute;
+  display: block;
+  width: 60px;
+  height: 2px;
+  background: #86c232;
+  bottom: 0;
+  left: calc(50% - 30px);
+}
+
+.bg-new {
+  transition: all 0.3s ease;
+  cursor: pointer;
+  background-color: #61892f;
+}
+.bg-new:hover {
+  transform: scale(1.04);
+}
+.bg-new-2 {
+  transition: all 0.3s ease;
+  cursor: pointer;
+  background-color: #f4df64;
+}
+.bg-new-2:hover {
+  transform: scale(1.04);
+}
+.nn img {
+  transition: all 0.3s ease;
+  cursor: pointer;
+}
+.nn img:hover {
+  transform: scale(1.04);
+}
+.uu li {
+  padding-top: 12px;
+  font-weight: 600;
+  color: #162746;
+  font-size: 15px;
+}
+h4.bg-heading {
+  background-color: #86c232;
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: 600;
+  display: inline-block;
+  border-radius: 20px;
+}
+h4.bg-heading2 {
+  background-color: #ffe131;
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: 600;
+  display: inline-block;
+  border-radius: 20px;
+}
+.contact-me {
+  margin-top: 150px;
+}
+.icon-box {
+  padding: 30px;
+  position: relative;
+  /* width: 100px;
+  height: 100px; */
+  overflow: hidden;
+  margin: 0 0 40px 0;
+  background: #fff;
+  box-shadow: 0 10px 29px 0 rgba(68, 88, 144, 0.1);
+  transition: all 0.3s ease-in-out;
+  border-radius: 15px;
+  text-align: center;
+  border-bottom: 3px solid #fff;
+}
+
+.icon-box:hover {
+  transform: translateY(-5px);
+  border-color: #86c232;
+}
+
+.icon i {
+  font-size: 48px;
+  line-height: 1;
+  margin-bottom: 15px;
+  color: #ef7f4d;
+}
+
+.title {
+  font-weight: 700;
+  margin-bottom: 15px;
+  font-size: 18px;
+}
+
+.title a {
+  color: #111;
+}
+
+.description {
+  font-size: 15px;
+  line-height: 28px;
+  margin-bottom: 0;
+}
+.line {
+  border-bottom: 10px solid #86c232;
+}
+.contain-shape1 {
+  padding: 100px;
+  position: relative;
+  background-color: #ffffff;
+  min-height: 100vh;
+  background-image: url(/images/back.jpg);
+  -webkit-background-size: cover !important;
+  -moz-background-size: cover !important;
+  -o-background-size: cover !important;
+  background-size: cover !important;
+  background-position: center center;
+  background-repeat: no-repeat;
+  image-rendering: -webkit-optimize-contrast;
+  -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+  filter: grayscale(100%);
+}
+
+.contain-shape2 {
+  position: relative;
+  background-color: #1e2528;
+  color: white;
+  min-height: 60vh;
+}
+
+.contain-shape3 {
+  position: relative;
+
+  min-height: 100vh;
+}
+.contain-shape4 {
+  position: relative;
+  background-color: #1e2528;
+
+  min-height: 50vh;
+}
+
+.contain-shape5 {
+  position: relative;
+
+  min-height: 90vh;
+}
+
+.cards {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  padding: 20px;
+  grid-gap: 40px;
+}
+.card {
+  background-color: #1c1b29;
+  border-radius: 20px;
+  box-shadow: 0 0 30px rgba(0, 0, 0, 0.18);
+}
+.container {
+  position: relative;
+  clip-path: polygon(0 0, 100% 0, 100% 85%, 0 100%);
+}
+.card img {
+  width: 100%;
+  display: block;
+  /* border-radius: 20px 20px 0 0; */
+}
+.container:after {
+  content: "";
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  /* border-radius: 20px 20px 0 0; */
+  opacity: 0.7;
+}
+.card1 > .container:after {
+  background-image: linear-gradient(135deg, #41b3a3, #c38d9e);
+}
+.card2 > .container:after {
+  background-image: linear-gradient(135deg, #2f2fa2, #f64c72);
+}
+.card3 > .container:after {
+  background-image: linear-gradient(135deg, #3500d3, #0c0032);
+}
+.details {
+  padding: 20px 10px;
+}
+.details > h3 {
+  color: #ffffff;
+  font-weight: 600;
+  font-size: 18px;
+  margin: 10px 0 15px 0;
+}
+.details > p {
+  color: #a0a0a0;
+  font-size: 15px;
+  line-height: 30px;
+  font-weight: 400;
+}
+
+@media only screen and (max-width: 600px) {
+  #portfolio-img {
+    display: none;
+  }
+  btn {
+    margin-top: 25px;
+  }
+}
+
+.contain-shape6 {
+  position: relative;
+  background-color: #1e2528;
+  min-height: 80vh;
+  padding: 50px;
+}
+
+.custom-shape-divider-bottom-1609841878 {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+  transform: rotate(180deg);
+}
+
+.custom-shape-divider-bottom-1609841878 svg {
+  position: relative;
+  display: block;
+  width: calc(100% + 1.3px);
+  height: 150px;
+}
+
+.custom-shape-divider-bottom-1609841878 .shape-fill {
+  fill: #1e2528;
+}
+
+.custom-shape-divider-bottom-1609841511 {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+  transform: rotate(180deg);
+}
+
+.custom-shape-divider-bottom-1609841511 svg {
+  position: relative;
+  display: block;
+  width: calc(100% + 1.3px);
+  height: 150px;
+  transform: rotateY(180deg);
+}
+
+.custom-shape-divider-bottom-1609841511 .shape-fill {
+  fill: #ffffff;
+}
+
+.custom-shape-divider-top-1609846653 {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+}
+
+.custom-shape-divider-top-1609846653 svg {
+  position: relative;
+  display: block;
+  width: calc(100% + 1.3px);
+  height: 150px;
+}
+
+.custom-shape-divider-top-1609846653 .shape-fill {
+  fill: #ffffff;
+}
+</style>
